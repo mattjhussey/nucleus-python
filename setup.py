f66f54940eafaa9a48f278d888c8d52c77a3f88d
Matt Hussey <matthew.hussey@googlemail.com>
2017-03-18 10:12:04 +0000

    Remove pytest configuration from tox. python setup.py runs tests inside the current environment. Tox runs test in multiple environments.

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ integration-with-setuptools-distribute-test-commands"""
 import os
 from setuptools import setup
 from setuptools import find_packages
-from setuptools.command.test import test as TestCommand
 
 
 def read(fname):
@@ -18,73 +17,8 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-venv_in_env = False
-
-
-class Tox(TestCommand):
-    """ Hook into running Tox for testing. """
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def __init__(self, dist):
-        TestCommand.__init__(self, dist)
-        self.tox_args = None
-        self.test_args = []
-        self.test_suite = True
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        
-        if not venv_in_env:
-            import virtualenv
-            from os.path import dirname
-            ve_path = dirname(virtualenv.__file__)
-            original_venv_create = tox.venv.tox_testenv_create
-            @tox.config.hookimpl
-            def wrap_venv_create(venv, action):
-                original_action_popen = action.popen
-                def wrap_action_popen(args, cwd, env, redirect, ignore_ret):
-                    try:
-                        env['PYTHONPATH'] += ';{0}'.format(ve_path)
-                    except:
-                        env['PYTHONPATH'] = ve_path
-                    return original_action_popen(
-                        args,
-                        cwd=cwd,
-                        env=env,
-                        redirect=redirect,
-                        ignore_ret=ignore_ret)
-                action.popen = wrap_action_popen
-                return original_venv_create(venv, action)
-            tox.venv.tox_testenv_create = wrap_venv_create
-                                      
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        else:
-            args = []
-        tox.cmdline(args=args)
-
-
 def main():
     """ Run the setup. """
-    try:
-        import virtualenv
-        global venv_in_env
-        venv_in_env = True
-    except ImportError:
-        pass
-
     setup(
         name="pybaseline",
         version="0.0.1",
@@ -105,8 +39,8 @@ def main():
         long_description=read("README"),
         classifiers=[
             "Development Status :: 1 - Planning"],
-        tests_require=['tox'],
-        cmdclass={"test": Tox},
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest', 'pytest-cov', 'robber']
     )
 
 if __name__ == "__main__":
